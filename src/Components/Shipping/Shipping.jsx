69b0c117418995bfa899db2fac56227514378aad
FoysalHossain2 <fh375170@gmail.com>
2024-03-22 22:20:06 +0600

    Shipping part modified

--- src/Components/Shipping/Shipping.jsx
+++ src/Components/Shipping/Shipping.jsx
@@ -4,17 +4,27 @@ import ShippingItems from '../CommonComponents/ShippingItems'
 import { FaTruck } from "react-icons/fa";
 
 const Shipping = () => {
+
+  const data = [
+    { id: 1, icon: 2, title: 'Two years warranty'},
+    { id: 1, icon: <FaTruck/>, title: 'Free shipping'},
+    { id: 1, icon: <FaTruck/>, title: 'Return policy in 30 days shipping'}
+  ]
+
   return (
     <> 
         <div className='py-[22px]'>
           <div className="container">
             <Flex className={'justify-between items-center'}>
-               {[...Array(3)].map(()=>(
-                  <ShippingItems className={'flex items-center gap-x-2' }>
-                    <span> <FaTruck/> </span>
-                     <p>Two years warranty</p>
+              {data?.map((item) => (
+                <ShippingItems
+                className={'flex items-center gap-x-3'}
+                key={item.id}
+                >
+                  <span className='text-xl font-bold'>{item.icon}</span>
+                  <p>{item.title}</p>
                 </ShippingItems>
-               ))}
+              ))}
             </Flex>
           </div>
         </div>
