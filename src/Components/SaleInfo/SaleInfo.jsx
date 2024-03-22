8eb94796af564e34b0efdfb84dcbf87841ceb035
FoysalHossain2 <fh375170@gmail.com>
2024-03-22 23:02:59 +0600

    transfer file

--- src/Components/SaleInfo/SaleInfo.jsx
+++ src/Components/SaleInfo/SaleInfo.jsx
@@ -16,17 +16,7 @@ const SaleInfo = () => {
                   <img src={Ad_1} alt="" />
                 </picture>
 
-                <div className='absolute bottom-[72px] left-16'>
-                  <h2 className='font-DM_Sans font-bold text-4xl text-main_text_color mb-9'>
-                    Phones Sale
-                  </h2>
-                  <p className='font-DM_Sans opacity-75 text-main_text_color mb-[49px]'>
-                    up to <span className='font-bold text-[#262626] text-[40px] p-3'>30%</span> sale for all phones!
-                  </p>
-                  <Button className={ 'inline-block bg-main_text_color py-4 px-16 text-white hover:bg-slate-800 cursor-pointer'}>
-                    Shop Now
-                  </Button>
-                </div>
+               
               </div>
 
               <div className='w-1/2 flex flex-col gap-10'>
