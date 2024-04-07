6d11db57ece3407f442ca777edf43bd10c952216
FoysalHossain2 <fh375170@gmail.com>
2024-04-08 00:18:45 +0600

    BreadCrump complite

--- src/Components/CommonComponents/BreadCrumb/BreadCrumb.jsx
+++ src/Components/CommonComponents/BreadCrumb/BreadCrumb.jsx
@@ -1,18 +1,29 @@
 import React from 'react'
 import { Link , useLocation} from 'react-router-dom'
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 
 const BreadCrumb = () => {
 
   const location = useLocation()
-  console.log(location);
-  
+  const BreadCrumbPathname = location.pathname.split('/').filter((x) => x)
 
   return (
     <>
      <div>
-        <ul>
-            <li> <Link to={'/'}>Home</Link> </li>
-            <li> <Link to={'/shop'}>Shop</Link> </li>
+        <ul className='flex items-center text-center gap-1'>
+            <li> 
+              <Link to={'/'}>Home</Link> 
+            </li>
+            <div className='text-[23px]'><MdOutlineKeyboardArrowRight /></div>
+            <li> 
+              {BreadCrumbPathname.map((path, index) => {
+              const isList = index === BreadCrumbPathname.length - 1;
+
+                return (
+                  <Link className={`${isList === true ? 'text-gr-400' : 'text-pink-900'}`} to={'/'}> {path}</Link> 
+                )
+              })}
+            </li>
         </ul>
      </div>
     </>
